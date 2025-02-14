d5b800111f27880a24ee8e47336b263764849944
Jyotin Goel <b22ai063@iitj.ac.in>
2025-02-14 13:57:26 +0530

    Backend | Placeholders
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- backend/generate.py
+++ backend/generate.py
@@ -1,2 +1,9 @@
-class VLM:
-    pass
+class ImageParsing:
+    def __init__(self):
+        pass
+
+    def yolo_inference_on_yt_video(self, vid_dir: str):
+        pass
+
+    def create_scene_description(self):
+        pass
