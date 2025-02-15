d78e67bd779cde55e0d97a37189228eaae547564
Jyotin Goel <b22ai063@iitj.ac.in>
2025-02-15 12:32:37 +0530

    Backend | SceneDescription
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- backend/generate.py
+++ backend/generate.py
@@ -5,11 +5,11 @@ import os
 import base64
 
 class ImageParsing:
-    def __init__(self, yolo_version: str, vlm_avail: str = "llava"):
+    def __init__(self, yolo_version: str, vlm_avail: str = "llava:7b-v1.5-q6_K"):
         self.yolo_model = YOLO(
             f"{yolo_version}n.pt"
         )
-        self.vlm = "llava:7b-v1.5-q6_K"
+        self.vlm = vlm_avail
 
     def convert_image(self, img_file):
         with open(img_file, "rb") as image:
@@ -18,23 +18,27 @@ class ImageParsing:
     def yolo_inference_on_yt_video(self, vid_dir: str, save_dir: str):
         img_dir = os.listdir(vid_dir)
         img_dir_fpath = [f"{vid_dir}/{img}" for img in img_dir]
+        os.makedirs(save_dir, exist_ok=True)
 
         results = self.yolo_model(
             img_dir_fpath, 
             stream=True, 
             save=True, 
             project=save_dir,
-            exist_ok=True
+            exist_ok=True,
+            conf=0.4
         )
 
-        for i, result in tqdm(enumerate(results)):
-            result.save(filename=f"{save_dir}result_{i}.jpg")
+        for i, result in enumerate(tqdm(results)):
+            video_id = img_dir[i].split('_')[0]
+            timestamp_jpg = img_dir[i].split('_')[-1]
+            result.save(filename=f"{save_dir}result_{video_id}_{timestamp_jpg}")
 
     def create_scene_description(self, prompt: str, img_file: str):
         base64_image = self.convert_image(img_file=img_file)
 
         payload = {
-            "model": f"{self.model}",
+            "model": f"{self.vlm}",
             "prompt": f"{prompt}",
             "stream": False,
             "images": [base64_image]
@@ -55,6 +59,6 @@ if __name__ == "__main__":
         "yolo11"
     )
     img_parse.yolo_inference_on_yt_video(
-        vid_url="https://www.youtube.com/watch?v=ftDsSB3F5kg",
+        vid_dir="./data/ftDsSB3F5kg",
         save_dir="./run_1/"
     )
