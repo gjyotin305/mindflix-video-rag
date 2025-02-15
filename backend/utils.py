a92dc8f8a5672dd6cbec329b3398cc889a1362a4
Jyotin Goel <b22ai063@iitj.ac.in>
2025-02-15 15:47:23 +0530

    Backend | VideoBreakDown | Complete Captions, Frames, DetectedFrames
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- backend/utils.py
+++ backend/utils.py
@@ -13,11 +13,34 @@ class VideoBreakDown:
     def __init__(self, backend: str = "decord"):
         self.backend = backend
 
+    def get_vid_transcript(
+        self, 
+        vid_url: str, 
+        save_dir: str="./data/",
+        lang: str = "hi"
+    ):
+        yt = YouTube(url=vid_url)
+        parsed_url = urlparse(
+            url=vid_url
+        )
+        video_id = parse_qs(parsed_url.query).get("v", [None])[0]
+        captions = yt.captions[f'a.{lang}']
+        os.makedirs(save_dir, exist_ok=True)
+        os.makedirs(f"{save_dir}{video_id}", exist_ok=True)
+
+        with open(f"{save_dir}{video_id}/{video_id}_captions.txt", "w") as f:
+            f.write(captions.generate_srt_captions())
+            f.close()
+
+        print(f"Saving {video_id}_captions.txt")
+
+
     def get_vid_to_frame_by_frame(
         self, 
         vid_url: str,
         save_dir: str = "./data/", 
-        every: int = 60):
+        every: int = 30
+    ):
         yt = YouTube(url=vid_url)
         parsed_url = urlparse(
             url=vid_url
@@ -53,5 +76,10 @@ class VideoBreakDown:
 
 if __name__ == "__main__":
     vid = VideoBreakDown()
-    vid.get_vid_to_frame_by_frame("https://www.youtube.com/watch?v=ftDsSB3F5kg")
+    vid.get_vid_to_frame_by_frame(
+        "https://www.youtube.com/watch?v=ftDsSB3F5kg"
+    )
+    vid.get_vid_transcript(
+        "https://www.youtube.com/watch?v=ftDsSB3F5kg"
+    )
 
